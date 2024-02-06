ecc1b3802c524cace9860775dab553b1a5f0a639
GiuliaZ538 <60102859+GiuliaZ538@users.noreply.github.com>
2024-02-06 17:35:30 +0100

    Update 0.4_SupplementaryMTG.R
    correcting code

--- 0.4_SupplementaryMTG.R
+++ 0.4_SupplementaryMTG.R
@@ -5,24 +5,26 @@ library(reshape2)
 library(ggplot2)
 library(gridExtra)
 ###########
-#make proportion tables
-count_table1 <- table(Eleph2650a$ED)
+#make count tables
+count_table1 <- table(Eleph2650$ED)
 print(count_table1)
 data_matrix1 <- as.matrix(count_table1)
 
-b1 <- prop.table(data.matrix(data_matrix1), margin=2) # makes proportion table, needs 2 margins e.g. header and 1st row names
-colSums(prop.table(b1, margin=2)) # should give 100 for each coloumn
-
-long_format1 <- melt(b2)
-long_format1 <- long_format1[, -2]
-colnames(long_format1) <- c("ED", "Read_prop")
-
-count_table2 <- table(Mamm2650a$ED)
+count_table2 <- table(Mamm2650$ED)
 print(count_table2)
 data_matrix2 <- as.matrix(count_table2)
 
-b2 <- prop.table(data.matrix(data_matrix2), margin=2) # makes proportion table, needs 2 margins
-colSums(prop.table(b2, margin=2)) # should give 100 for each coloumn
+#make proportion tables
+b1 <- prop.table(data.matrix(data_matrix1), margin=2)
+colSums(prop.table(b1, margin=2)) # should give 100 for each column
+
+b2 <- prop.table(data.matrix(data_matrix2), margin=2) 
+colSums(prop.table(b2, margin=2)) # should give 100 for each column
+
+#transposing to long format table for ggplot
+long_format1 <- melt(b1)
+long_format1 <- long_format1[, -2]
+colnames(long_format1) <- c("ED", "Read_prop")
 
 long_format2 <- melt(b2)
 long_format2 <- long_format2[, -2]
@@ -31,10 +33,10 @@ colnames(long_format2) <- c("ED", "Read_prop")
 long_format1_Ele <- cbind(long_format1, Taxa = "Elephant")
 long_format2_Mamm <- cbind(long_format2, Taxa = "Mammoth")
 
-#join data Mammuth-Elephant reads
+#join data Mammoth-Elephant reads
 BindTable_E_M <- bind_rows(long_format1_Ele, long_format2_Mamm)
 
-#combined plots
+#make combined plots
 pdf(file = "Read_prop_ED_all.pdf", width = 4, height = 6)
 ggplot(BindTable_E_M, aes(x=ED , y=Read_prop)) +
   geom_path(aes(col=Taxa))+
@@ -44,7 +46,7 @@ ggplot(BindTable_E_M, aes(x=ED , y=Read_prop)) +
   theme_minimal() + theme(aspect.ratio=10/2)
 dev.off()
 
-#single plots
+#or make single plots
 p1 <- ggplot(long_format1, aes(x = ED, y = Read_prop)) +
   geom_line(color = "blue", size = 1) +
   geom_point(color = "blue", size = 3) +  # Add points (optional)
